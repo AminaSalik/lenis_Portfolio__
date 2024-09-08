e9de8ed421cf12b4483b0d9a6f132c9aea5ed2ab
AminaSalik <aminasalik012@gmail.com>
2024-09-09 00:06:55 +0100

    dn

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,49 +1,55 @@
-// Preload images
+/**
+ * Preloads images specified by the CSS selector.
+ * @function
+ * @param {string} [selector='img'] - CSS selector for target images.
+ * @returns {Promise} - Resolves when all specified images are loaded.
+ */
 const preloadImages = (selector = 'img') => {
     return new Promise((resolve) => {
+        // The imagesLoaded library is used to ensure all images (including backgrounds) are fully loaded.
         imagesLoaded(document.querySelectorAll(selector), {background: true}, resolve);
     });
 };
 
+/**
+ * Linear interpolation
+ * @param {Number} a - first value to interpolate
+ * @param {Number} b - second value to interpolate 
+ * @param {Number} n - amount to interpolate
+ */
+const lerp = (a, b, n) => (1 - n) * a + n * b;
 
-// Helper function that lets you dynamically figure out a grid's rows/columns as well as further refine those with "odd" or "even" ones
-// https://greensock.com/forums/topic/34808-how-can-i-animate-the-odd-and-even-columns-rows-of-a-grid-with-gsapto/?do=findComment&comment=174346
-const getGrid = selector => {
-	let elements = gsap.utils.toArray(selector),
-		bounds,
-		getSubset = (axis, dimension, alternating, merge) => {
-		  	let a = [], 
-			  	subsets = {},
-			  	onlyEven = alternating === "even",
-			  	p;
-			bounds.forEach((b, i) => {
-				let position = Math.round(b[axis] + b[dimension] / 2),
-					subset = subsets[position];
-				subset || (subsets[position] = subset = []);
-				subset.push(elements[i]);
-			});
-			for (p in subsets) {
-				a.push(subsets[p]);
-			}
-			if (onlyEven || alternating === "odd") {
-				a = a.filter((el, i) => !(i % 2) === onlyEven);
-			}
-		  	if (merge) {
-				let a2 = [];
-				a.forEach(subset => a2.push(...subset));
-				return a2;
-		  	}
-		  	return a;
-		};
-	elements.refresh = () => bounds = elements.map(el => el.getBoundingClientRect());
-	elements.columns = (alternating, merge) => getSubset("left", "width", alternating, merge);
-	elements.rows = (alternating, merge) => getSubset("top", "height", alternating, merge);
-	elements.refresh();
+ /**
+  * Gets the cursor position
+  * @param {Event} ev - mousemove event
+  */
+const getCursorPos = ev => {
+     return { 
+         x : ev.clientX, 
+         y : ev.clientY 
+     };
+ };
 
-	return elements;
+/**
+ * Map number x from range [a, b] to [c, d] 
+ */
+const map = (x, a, b, c, d) => (x - a) * (d - c) / (b - a) + c;
+
+/**
+ * Calculates the viewport size
+ */
+const calcWinsize = () => {
+    return {
+        width: window.innerWidth, 
+        height: window.innerHeight
+    }
 }
 
+// Exporting utility functions for use in other modules.
 export {
     preloadImages,
-    getGrid,
+    lerp,
+    getCursorPos,
+    map,
+    calcWinsize
 };
